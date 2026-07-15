e907657f2aa36aef96289504d1bba6ebf118ac81
Tom <35542432+AquaBx@users.noreply.github.com>
2026-07-15 23:16:30 +0200

    compress

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter(), alias:{'$content/*':"src/content/*"} },
+	kit: { adapter: adapter({ precompress: false }), alias:{'$content/*':"src/content/*"} },
 	extensions: ['.svelte', '.svx']
 };
 
